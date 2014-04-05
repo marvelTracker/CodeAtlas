32692e0fcfb10a4de6c4dbc90212459caece27b6
dinesh <dineshpdn@hotmail.com>
2014-04-05 23:20:46 +1100

    add rep

---
+++ ServiceMate.Repository/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarMate.Repository
+{
+    public interface IGenericRepository<TEntity>
+    {
+
+        Task<TEntity> GetByIdAsync(int id);
+
+        IQueryable<TEntity> SearchFor(Expression<Func<TEntity, bool>> predicate);
+
+        IQueryable<TEntity> GetAll();
+
+        Task EditAsync(TEntity entity);
+
+        Task InsertAsync(TEntity entity);
+
+        Task DeleteAsync(TEntity entity);
+    } 
+}
