315da4c0406bb1ca003c8817f8cedccece990369
marveltracker <dineshranasinghe@gmail.com>
2014-04-10 22:30:39 +1000

    git add-commit

---
+++ ServiceMate.Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceMate.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string EmailId { get; set; }
+        public string Name { get; set; }
+        public UserTypeModel UserType { get; set; }
+    }
+} 
---
+++ ServiceMate.Models/UserTypeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceMate.Models
+{
+    public enum  UserTypeModel
+    {
+        Admin = 0,
+        ServiceProvider = 1,
+        Consumer = 2,
+    }
+}
